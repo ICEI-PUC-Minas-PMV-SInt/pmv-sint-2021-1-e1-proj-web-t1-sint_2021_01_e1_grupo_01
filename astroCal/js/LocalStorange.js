fadf1b8323a63e333a191b1c728d139143349e93
Erick Ribeiro <gustavoribeiroerick@gmail.com>
2021-05-18 20:43:04 -0300

    :construction: implementacao do card 19

---
+++ astroCal/js/LocalStorange.js
@@ -0,0 +1,28 @@
+var localStorage = Window.localStorage;
+
+function saveEvent() {
+    var newEvent = new Object();
+
+    newEvent.id = CreateGuid();
+    newEvent.name = document.getElementById("name").value;
+    newEvent.short_descripton = document.getElementById("short_descripton").value;
+    newEvent.data_event = document.getElementById("data_event").value;
+    newEvent.location = document.getElementById("location").value;
+    newEvent.tag = document.getElementById("tag").value;
+    newEvent.photo = document.getElementById("photo").value;
+    newEvent.short_descripton2 = document.getElementById("short_descripton2").value;
+
+    var stageLocalStorage = JSON.stringify(newEvent);
+
+    localStorage.setItem(newEvent.id, stageLocalStorage);
+}
+
+
+
+function CreateGuid(){
+    function _p8(s) {  
+        var p = (Math.random().toString(16)+"000000000").substr(2,8);  
+        return s ? "-" + p.substr(0,4) + "-" + p.substr(4,4) : p ;  
+     }  
+     return _p8() + _p8(true) + _p8(true) + _p8();
+}
---
+++ astroCal/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $('.modal').modal();
+});
+
+$(document).ready(function () {
+    $('.datepicker').datepicker();
+});
+
+$('.chips-placeholder').chips({
+    placeholder: "Adicione palavras chaves para a ocorrência",
+    secondaryPlaceholder: '+Tag',
+});
+
+
+
+

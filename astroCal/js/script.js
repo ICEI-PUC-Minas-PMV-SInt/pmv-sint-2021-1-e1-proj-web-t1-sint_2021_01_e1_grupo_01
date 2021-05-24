118e68147c2566985b2ad211457871728544ad74
Erick Ribeiro <gustavoribeiroerick@gmail.com>
2021-05-24 14:32:13 -0300

    :building_construction: equalizando a branch feature/develop

---
+++ astroCal/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.modal').modal();
+});
+
+$(document).ready(function () {
+    $('.datepicker').datepicker();
+});
+
+$('.chips-placeholder').chips({
+    placeholder: "Adicione palavras chaves para a ocorrência",
+    secondaryPlaceholder: '+Tag',
+});

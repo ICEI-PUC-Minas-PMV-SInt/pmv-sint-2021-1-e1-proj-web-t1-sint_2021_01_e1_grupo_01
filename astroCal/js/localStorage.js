984bb40f4449f2726d878231dc5185d6e997df6b
Paulo Henrique Moura Da Silva <paulomoura.92@hotmail.com>
2021-05-25 22:00:24 -0300

    Melhoria do codigo

--- astroCal/js/localStorage.js
+++ astroCal/js/localStorage.js
@@ -2,20 +2,21 @@ var localStorage = Window.localStorage;
 
 function saveEvent() {
     var newEvent = new Object();
+    var numberTag;
+    var tags = [];
 
     newEvent.id = CreateGuid();
     newEvent.name = document.getElementById("name").value;
     newEvent.short_descripton = document.getElementById("short_descripton").value;
     newEvent.data_event = document.getElementById("data_event").value;
     newEvent.location = document.getElementById("location").value;
-    var i;
-    var tags = [];
-    for (i = 0; i < document.querySelectorAll('[id="tag"]')[0].getElementsByTagName('div').length; i++) {
-        tags.push(document.querySelectorAll('[id="tag"]')[0].getElementsByTagName('div')[i].innerText.replace("close","").replace(/(\r\n|\n|\r)/gm, ""));
-    }
-    newEvent.tag = tags;
     newEvent.photo = document.getElementById("photo").value;
     newEvent.full_descripton = document.getElementById("full_descripton").value;
+
+    for (numberTag = 0; numberTag < document.querySelectorAll('[id="tag"]')[0].getElementsByTagName('div').length; numberTag++) {
+        tags.push(document.querySelectorAll('[id="tag"]')[0].getElementsByTagName('div')[numberTag].innerText.replace("close","").replace(/(\r\n|\n|\r)/gm, ""));
+    }
+    newEvent.tag = tags;
 
     var stageLocalStorage = JSON.stringify(newEvent);
 

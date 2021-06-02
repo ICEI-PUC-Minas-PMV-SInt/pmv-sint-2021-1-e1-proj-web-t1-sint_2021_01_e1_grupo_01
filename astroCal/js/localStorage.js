a7d6b06afb5179898872f9550369fbbc22f70878
alcpl01 <lindinaluli12@gmail.com>
2021-06-01 21:06:08 -0300

    :construction:

--- astroCal/js/localStorage.js
+++ astroCal/js/localStorage.js
@@ -5,10 +5,10 @@ function saveEvent() {
 
     newEvent.id = CreateGuid();
     newEvent.name = document.getElementById("name").value;
-    newEvent.short_descripton = document.getElementById("short_descripton").value;
-    newEvent.data_event = document.getElementById("data_event").value;
-    newEvent.location = document.getElementById("location").value;
-    newEvent.tag = document.getElementById("tag").value;
+    newEvent.short_descripton = document.getElementById("short_description").value;
+    //newEvent.data_event = document.getElementById("data_event").value;
+    //newEvent.location = document.getElementById("location").value;
+    //newEvent.tag = document.getElementById("tag").value;
     newEvent.photo = document.getElementById("photo").value;
     newEvent.full_descripton = document.getElementById("full_descripton").value;
 
@@ -19,37 +19,31 @@ function saveEvent() {
 
 
 
-function CreateGuid(){
-    function _p8(s) {  
-        var p = (Math.random().toString(16)+"000000000").substr(2,8);  
-        return s ? "-" + p.substr(0,4) + "-" + p.substr(4,4) : p ;  
-     }  
-     return _p8() + _p8(true) + _p8(true) + _p8();
+function CreateGuid() {
+    function _p8(s) {
+        var p = (Math.random().toString(16) + "000000000").substr(2, 8);
+        return s ? "-" + p.substr(0, 4) + "-" + p.substr(4, 4) : p;
+    }
+    return _p8() + _p8(true) + _p8(true) + _p8();
 }
 
 function formarCard() {
 
-    var currentId = localStorage.getItem('id');
-    //var currentTag = localStorage.getItem('tag');
-    //var currentFullDescription = localStorage.getItem('full_description');
+   /* var currentId = localStorage.getItem('id');
     var currentName = localStorage.getItem('name');
     var currentShortDescription = localStorage.getItem('short_description');
-    var currentDate = localStorage.getItem('data_event');
-    var currentLocation = localStorage.getItem('location');
-    var currentPhoto = localStorage.getItem('photo');
+    var currentPhoto = localStorage.getItem('photo'); */
 
 
-    if (currentId != null) {
-        for (let i = 0; i < localStorage.length; i++) {
 
-            console.log(document.getElementById('name')[i].value = currentName);
-            console.log(document.getElementById('short_description')[i].value = currentShortDescription);
-            console.log(document.getElementById('data_event')[i].value = currentDate);
-            console.log(document.getElementById('location')[i].value = currentLocation);
-            console.log(document.getElementById('photo')[i].value = currentPhoto);
-        }
-    }
-    else {
-        console.log("Nenhum dado foi encontrado.");
-    }
+    atual=document.getElementById("relatorio"); 
+    var html=""; 
+    var keys = Object.keys(localStorage);
+    keys.forEach(function(chave, pos){ 
+        let id = JSON.parse(localStorage.getItem(chave)); 
+        html = html+id.full_descripton; 
+    
+    });
+    document.getElementById("corpoTabela").innerHTML=html;
+
 }

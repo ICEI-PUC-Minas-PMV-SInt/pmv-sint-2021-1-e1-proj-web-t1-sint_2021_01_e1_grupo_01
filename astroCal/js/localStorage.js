ec7657256dcee573be79efa6147aac23c30e0b43
Déborah <deborahfsouzadfs@gmail.com>
2021-06-04 19:54:19 -0300

    :memo: Criação da navbar

--- astroCal/js/localStorage.js
+++ astroCal/js/localStorage.js
@@ -39,3 +39,23 @@ function CreateGuid(){
      }  
      return _p8() + _p8(true) + _p8(true) + _p8();
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const darkModeStorage = localStorage.getItem('dark-mode')
+    const html = document.querySelector('html')
+    const inputDarkMode = document.getElementById('input-dark-mode')
+
+    if(darkModeStorage){
+        html.setAttribute("dark", "true")
+    }
+
+    inputDarkMode.addEventListener('change', () => {
+        if(inputDarkMode.checked){
+            html.setAttribute("dark", "true")
+            localStorage.setItem('dark-mode', true)
+        }else{
+            html.removeAttribute("dark")
+            localStorage.removeItem('dark-mode')
+        }
+   })
+})
--- astroCal/js/script.js
+++ astroCal/js/script.js
@@ -10,3 +10,16 @@ $('.chips-placeholder').chips({
     placeholder: "Adicione palavras chaves para a ocorrência:",
     secondaryPlaceholder: '+Tag',
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const html = document.querySelector('html')
+    const inputDarkMode = document.getElementById('input-dark-mode')
+				
+    inputDarkMode.addEventListener('change', () => {
+        if(inputDarkMode.checked){
+            html.setAttribute("dark", "true")
+        }else{
+            html.removeAttribute("dark")
+        }
+    })
+})

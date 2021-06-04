b161282ac8000f7e29bced3c2c7eba5472ffcfd9
alcpl01 <lindinaluli12@gmail.com>
2021-06-04 20:28:28 -0300

    :construction: Alteração DarkMode

--- astroCal/js/script.js
+++ astroCal/js/script.js
@@ -11,15 +11,15 @@ $('.chips-placeholder').chips({
     secondaryPlaceholder: '+Tag',
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    const html = document.querySelector('html')
-    const inputDarkMode = document.getElementById('input-dark-mode')
-				
-    inputDarkMode.addEventListener('change', () => {
-        if(inputDarkMode.checked){
-            html.setAttribute("dark", "true")
-        }else{
-            html.removeAttribute("dark")
-        }
-    })
-})
+const toggle = document.getElementById('toggle');
+const body = document.body;
+
+toggle.addEventListener('input', e => {
+    const isChecked = e.target.checked;
+
+    if (isChecked) {
+        body.classList.add('dark-theme');
+    } else {
+        body.classList.remove('dark-theme');
+    }
+});
